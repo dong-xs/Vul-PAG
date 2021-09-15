a3edc05f189d42a7d4012650d494e729f0f5116c
dong-xs <461262373@qq.com>
2021-09-15 09:42:43 +0800

    merge-label.txt changed

--- bert/bert_word_embedding.py
+++ bert/bert_word_embedding.py
@@ -1,6 +1,7 @@
 #encoding:utf-8
 #bert的tensorflow安装可参照链接：https://cloud.tencent.com/developer/news/492066
 #BERT启动命令：bert-serving-start -model_dir I:\cased_L-12_H-768_A-12 -num_worker=1
+#该模型参数的意思是12个layers，768的hidden-dim，12个head的attention
 
 from bert_serving.client import BertClient
 client=BertClient()
